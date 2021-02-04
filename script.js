596a3088c56d6c13449b6e512132220dc91a2414
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-02-04 01:48:00 -0500

    Added console.logs

--- script.js
+++ script.js
@@ -3,27 +3,44 @@ var generateBtn = document.querySelector("#generate");
 
 var letterU = "qwertyuiopasdfghjklzxcvbnm";
 var letterL = "QWERTYUIOPASDFGHJKLZXCVBNNM";
-var number = "0123456789";
-var specialCharacter = "!@#$%^&*()_+=-[]|{}/\\'~<>";
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacter = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var lengthPassword = ""
 
 
 // Write password to the #password input
 function writePassword() {
-  var confirmUppercase = confirm("Uppercase letters?");
-  var confirmLowercase = confirm("Lowercase letters?");
-  var confirmNumber = confirm("Number?");
-  var confirmSpecial = confirm("Special Characters?");
-  var lengthPassword = prompt("How long do you want the password to be?");
+  var confirmUppercase = confirm("Uppercase letters?")
+  console.log(confirmUppercase);
 
+  var confirmLowercase = confirm("Lowercase letters?")
+  console.log(confirmLowercase);
 
-  var password = generatePassword(confirmUppercase, confirmLowercase, confirmNumber, confirmSpecial, lengthPassword);
-  
+  var confirmNumber = confirm("Number?")
+  console.log(number);
 
-  var passwordText = document.querySelector("#password");
+  var confirmSpecial = confirm("Special Characters?")
+  console.log(specialCharacter);
 
-  passwordText.value = password;
+  var lengthPassword = prompt("How long do you want the password to be?")
+  console.log(specialCharacter);
+}
+
+function generatePassword() {
 
 }
+
+// Math.random().toString()
+
+var password = generatePassword()
+
+
+
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
